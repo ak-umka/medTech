8c64763fd0fc21c585f2f6f60d2eaf0e90660668
ak-umka <umbet70@gmail.com>
2022-12-01 11:24:11 +0600

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  head: {
+    title: 'medTech-ht',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  ssr: true,
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["@/assets/scss/app.css"],
+
+  plugins: [
+    { src: "~/plugins/vuelidate" },
+  ],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  build: {},
+}
