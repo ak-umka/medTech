1fca253d06d2325f5dcb16dc5c04e1cd15586e69
ak-umka <umbet70@gmail.com>
2022-12-03 03:40:37 +0600

    front auth in progress

---
+++ front-nuxt/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <Header />
+        <Nuxt  />
+      </div>
+    </div>
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutPage',
+}
+</script>
