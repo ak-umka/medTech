99bc40408b8c234e7284a4d5231826d0aa90670d
ak-umka <umbet70@gmail.com>
2022-12-04 15:44:18 +0600

    fix model

--- back/models/inspection.js
+++ back/models/inspection.js
@@ -1,53 +1,125 @@
 import mongoose from "mongoose";
 
 const inspectionSchema = new mongoose.Schema({
-    complaints: [{                                  // Жалобы при поступлении
-        type: String,
-        required: true
-    }],
-    medicalHistory: [{                              // Анамнез заболевания
-        type: String,
-        required: true
-    }],
+    complaints: {                                  // Жалобы при поступлении
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    medicalHistory: {                              // Анамнез заболевания
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
+    },
     AnamnesisOfLife: {                             //  Анамнез жизни
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     AllergologicalHistory: {                       // Аллергологический анамнез
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     PurposeOfHospitalization: {                    // Цель госпитализации
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     lastSurvey: {                                  // Последний обследование
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     objectiveData: {                               // Объективные данные
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     survey: {                                      // Обследование
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     treatmentPlan: {                               // Лечебный план
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     other: {                                       // Прочее
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     underlyingDisease: {                           // Основное заболевание
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     concomitantDisease: {                          //Сопутствующее заболевание
-        type: String,
-        required: true
+        tags: [{
+            type: String,
+            required: true
+        }],
+        text: {
+            type: String,
+            required: true
+        }
     },
     doctor: {                                      //Врач
         type: mongoose.Schema.Types.ObjectId,
